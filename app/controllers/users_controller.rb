c7e1420fcd6b7efce2d36008fa5557db29561baf
hase75 <122374696+hase75@users.noreply.github.com>
2023-02-15 15:25:07 +0000

    [Finish]4-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
+  
+  def index
+    @user = 
+  
+  end
+  
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
   end
+  
+  
+  private
+  
+  def user_params_update
+    params.require(:user).permit(:name, :image, :introduction)
+  end
 end
+
+ 
