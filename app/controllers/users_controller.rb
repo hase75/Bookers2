887f5e284b5aa7f963bd36dc6073630cc451967b
hase75 <122374696+hase75@users.noreply.github.com>
2023-02-22 11:33:24 +0000

    [Start]4-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+   if @user.save
     redirect_to user_path(@user.id)
+   else
+    render :edit
+   end
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,12 @@
 class Book < ApplicationRecord
 
   belongs_to :user
-  
+
   has_one_attached :profile_image
-  
+
+  validates :title, presence: true
+
+  validates :body, presence: true, length: {maximum: 200}
 
   def get_profile_image(width, height)
   unless profile_image.attached?
@@ -12,5 +15,5 @@ class Book < ApplicationRecord
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
